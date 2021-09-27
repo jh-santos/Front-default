4088819d8484a1560ee81161d55343ab7086301c
João Henrique <jhsantos.cwb@gmail.com>
2021-09-27 11:32:23 -0300

    melhorias e modularização de componentes
    melhoria na base do projeto, visando mais facilidade na inclusão de itens

--- config/header.php
+++ config/header.php
@@ -5,10 +5,12 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Front</title>
-   <link rel="stylesheet" href="assets/assets/bootstrap/dist/css/bootstrap.min.css">
-   <link rel="stylesheet" href="assets/assets/slick-carousel/slick/slick.css">
-   <link rel="stylesheet" href="assets/assets/slick-carousel/slick/slick-theme.css">
-   <link rel="stylesheet" href="assets/assets/lity/dist/lity.min.css">
+   <!-- requisitos -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/animate.min.css">
+   <link rel="stylesheet" href="assets/css/lity.min.css">
+   <link rel="stylesheet" href="assets/includes/slick/slick.css">
+   <!-- custom  -->
    <link rel="stylesheet" href="assets/css/custom.css">
 
 </head>
